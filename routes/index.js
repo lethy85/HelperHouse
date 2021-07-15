545f895d75ba4824d9cfe0c1c588a153ce9cbee8
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-07-15 01:03:42 -0300

    adicionar css false

--- routes/index.js
+++ routes/index.js
@@ -15,15 +15,15 @@ router.get('/como-funciona', function(req, res, next) {
 });
 
 router.get('/para-quem-contrata', function(req, res, next) {
-  res.render('para-quem-contrata', { title: 'Contratar Serviço', logged: false });
+  res.render('para-quem-contrata', { title: 'Contratar Serviço', logged: false, style: false });
 });
 
 router.get('/para-profissional', function(req, res, next) {
-  res.render('para-profissional', { title: 'Prestar Serviço', logged: false });
+  res.render('para-profissional', { title: 'Prestar Serviço', logged: false, style: false });
 });
 
 router.get('/dashboard-prestador', function(req, res, next) {
-  res.render('dashboard-prestador', { title: 'Dashboard Prestador', logged: true });
+  res.render('dashboard-prestador', { title: 'Dashboard Prestador', logged: true, style: false });
 });
 // rotas adicionais para cada página deve ser criada aqui
 //router.get('/nome-da-pagina', function(req, res, next) {
