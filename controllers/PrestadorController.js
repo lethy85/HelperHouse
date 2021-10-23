d7ed181b1a0da52af7ac3b867ea3728ba15eea98
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-10-23 08:04:37 -0300

    feat(pkg): add multer and bcryptjs

--- controllers/PrestadorController.js
+++ controllers/PrestadorController.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs')
 const prestadorModel = require('../models/prestadorModels');
 
 exports.cadastrarPrestador = ({nome, sobrenome, email, senha, confsenha, endereco, CEP, telefone, registro, foto, ident}) => prestadorModel.cadastrarPrestador({nome, sobrenome, email, senha, confsenha, endereco, CEP, telefone, registro, foto, ident});
--- controllers/TomadorController.js
+++ controllers/TomadorController.js
@@ -1,4 +1,5 @@
+const bcrypt = require('bcryptjs')
 const tomadorModel = require('../models/tomador');
 
-exports.cadastrarTomador = ({nome, sobrenome, email, telefone, endereco, senha, confsenha}) =>
-    tomadorModel.cadastrarTomador({nome, sobrenome, email, telefone, endereco, senha, confsenha});
+exports.cadastrarTomador = ({nome, sobrenome, email, telefone, endereco, senha}) =>
+    tomadorModel.cadastrarTomador({nome, sobrenome, email, telefone, endereco, senha});
